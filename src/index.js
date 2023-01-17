6ed901de71208d1bd77c468c2aaeed22ba008a72
felixDev22 <felixnzomo@gmail.com>
2023-01-18 01:57:02 +0300

    Install bootstrap, import bootstrap to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // component file
 
