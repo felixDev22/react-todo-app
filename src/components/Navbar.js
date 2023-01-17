2a11c19c7cabeb02bd52a18b85619e3da25c170f
felixDev22 <felixnzomo@gmail.com>
2023-01-18 02:34:55 +0300

    Add class not active for the nav-section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 import menu from '../images/menu-bar.svg';
+import close from '../images/close.svg';
 
 const Navbar = () => {
   return (
     <nav className="Nav-bar">
       <div className="Menu-bar">
-        <img src={menu} alt="menu" />
+        <img
+          src={menu}
+          alt="menu"
+        />
+        <img
+          className="close not-active"
+          src={close}
+          alt="close"
+        />
       </div>
     </nav>
   );
