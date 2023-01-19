4c4e115b6a9cf1c748225059cfdcee21db155230
felixDev22 <felixnzomo@gmail.com>
2023-01-19 12:15:24 +0300

    Update Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,64 @@
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import menu from '../images/menu-bar.svg';
 import close from '../images/close.svg';
 
+const links = [
+  {
+    id: 1,
+    path: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    path: '/about',
+    text: 'About',
+  },
+];
+
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <nav className="Nav-bar">
-      <img
-        className="menu"
-        src={menu}
-        alt="menu"
-      />
-      <div className="Menu-pop not-active">
+      <button
+        onClick={handleToggle}
+        type="button"
+      >
+        <img
+          className="menu"
+          src={menu}
+          alt="menu"
+        />
+      </button>
+      <div className={`Menu-pop ${navbarOpen ? ' Menu-pop' : ''}`}>
         <img
           className="close"
           src={close}
           alt="close"
         />
         <ul className="Menu-links">
-          <li>HOME</li>
-          <li>ABOUT</li>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink
+                to={link.path}
+                activeClassName="active"
+                onClick={() => closeMenu()}
+                exact
+              >
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
